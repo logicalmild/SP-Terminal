683bdbf7bd64edaba6d0a180feffe6cceb27953d
Saranchai Anunthananaruporn <guide123@windowslive.com>
2019-10-28 01:08:13 +0700

    t

--- js/terminal.js
+++ js/terminal.js
@@ -265,47 +265,7 @@ var terminal = $('#term_demo').terminal(function(command) {
 
 
 
-function GetList(ListName){
 
-    var text = '\n';
-
-    
-    $.ajax({
-        url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists',
-        type: 'GET',
-        async: false,
-        headers: {
-        'accept': 'application/json;odata=verbose',
-        'content-type': 'application/json;odata=verbose',
-        },
-        success: function (data) {
-            var temp = data.d.results;
-            
-            if(ListName == 'title'){
-                for(i=0 ; i < temp.length ; i++){
-                    text += temp[i].Title + '\n';
-                }
-            }else{
-                for(i=0 ; i < temp.length ; i++){
-                    if(ListName.toUpperCase() == temp[i].Title.toUpperCase()){
-                        text += PackTextRow(40,temp[i]) + '\n';
-                    }else if(i == (temp.length-1)){
-                        text = 'List ' + ListName + ' is not found.\n';
-
-                    }
-                    
-                }
-            }   
-            
-        },
-
-        error: {}
-    
-    });
-
-
-    return text;
-}
 
 
 
--- module/getlist/getlist.js
+++ module/getlist/getlist.js
@@ -1,20 +1,41 @@
 function GetList(){
-    var text = '';
-    if(command == 'GET LIST'){
+    var text = '\n';
 
-        text = GetList('title');
-        // terminal.echo(text);
-
-    }
-    else{
-
-        var ListName = command.split(' ');
-        ListName.splice(0,2);
-        ListName = ListName.toString();
-        ListName = ListName.replace(/,/g,' ');
-        text = GetList(ListName);
-        // terminal.echo(text);
     
-    }
+    $.ajax({
+        url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists',
+        type: 'GET',
+        async: false,
+        headers: {
+        'accept': 'application/json;odata=verbose',
+        'content-type': 'application/json;odata=verbose',
+        },
+        success: function (data) {
+            var temp = data.d.results;
+            
+            if(ListName == 'title'){
+                for(i=0 ; i < temp.length ; i++){
+                    text += temp[i].Title + '\n';
+                }
+            }else{
+                for(i=0 ; i < temp.length ; i++){
+                    if(ListName.toUpperCase() == temp[i].Title.toUpperCase()){
+                        text += PackTextRow(40,temp[i]) + '\n';
+                    }else if(i == (temp.length-1)){
+                        text = 'List ' + ListName + ' is not found.\n';
+
+                    }
+                    
+                }
+            }   
+            
+        },
+
+        error: {}
+    
+    });
+
+
     return text;
 }
+
