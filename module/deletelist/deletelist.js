5e1e79dbf4ddd363b342fa768fa23f401707e881
Saranchai Anunthananaruporn <guide123@windowslive.com>
2019-10-27 23:46:49 +0700

    ccc

--- module/deletelist/deletelist.js
+++ module/deletelist/deletelist.js
@@ -1,18 +1,14 @@
-
-function CreateList() {
+function Delete() {
     var Listname = '';
     terminal.push(function(command2) {
     
         Listname = command2;
-        
         var clientContext = new SP.ClientContext(SiteUrl);
         var oWebsite = clientContext.get_web();
-        var listCreationInfo = new SP.ListCreationInformation();
-        listCreationInfo.set_title(Listname);
-        listCreationInfo.set_templateType(SP.ListTemplateType.announcements);
-        // this.oList = oWebsite.get_lists().add(listCreationInfo);
-        var oList = oWebsite.get_lists().add(listCreationInfo);
-        clientContext.load(oList);
+        var listTitle = Listname;
+        var oList = oWebsite.get_lists().getByTitle(listTitle);
+        oList.deleteObject();
+
         clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));
             
     }, {
@@ -22,7 +18,7 @@ function CreateList() {
 
 function onQuerySucceeded() {
     // var result = oList.get_title() + ' created.';
-    terminal.echo('Successful');
+    terminal.echo('Delete Successful');
 
 }
 
