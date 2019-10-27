bde66bb855056578e103f286850b32caab45d7c3
Saranchai Anunthananaruporn <guide123@windowslive.com>
2019-10-28 01:43:47 +0700

    ttt

--- module/getlistinfo/getlistinfo.js
+++ module/getlistinfo/getlistinfo.js
@@ -1,47 +1,35 @@
 function GetListInfo(){
-    var Listname = '';
-    var text;
-    terminal.push(function(command2) {
-        Listname = command2;
-        var text = '\n';
-        $.ajax({
-            url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists',
-            type: 'GET',
-            async: false,
-            headers: {
-            'accept': 'application/json;odata=verbose',
-            'content-type': 'application/json;odata=verbose',
-            },
-            success: function (data) {
-                data = data.d.results;
+    var Listname = 'ATC_Interview';
+    var text = '\n';
+    $.ajax({
+        url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists',
+        type: 'GET',
+        async: false,
+        headers: {
+        'accept': 'application/json;odata=verbose',
+        'content-type': 'application/json;odata=verbose',
+        },
+        success: function (data) {
+            data = data.d.results;
 
-                for(i in data){
+            for(i in data){
 
-                    if(data[i].Title == Listname){
-                        text = JSON.stringify(data[i], null , 2);
-                        text = text.replace(/"/g,'');
-                        text = text.replace(/,/g,'');
-                        terminal.echo(text);
-                   
-                    }
-                    
-            
+                if(data[i].Title == Listname){
+                    text = JSON.stringify(data[i], null , 2);
+                    text = text.replace(/"/g,'');
+                    text = text.replace(/,/g,'');
+                    terminal.echo(text);
+                
                 }
-
-                // text = JSON.stringify(data, null , 2);
-                // text = text.replace(/"/g,'');
-                // text = text.replace(/,/g,'');
-                // terminal.echo(text);
+                
         
-            },
+            }
 
-            error: {}
-        
-        });
+        },
 
-    }, {
-        prompt: 'List name > '
-    }); 
+        error: {}
+    
+    });
 
 
     return text;
