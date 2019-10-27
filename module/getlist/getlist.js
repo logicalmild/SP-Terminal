6a7c0d09aa1ade14cafc53229abd76f557eb8778
Saranchai Anunthananaruporn <guide123@windowslive.com>
2019-10-28 01:10:01 +0700

    ttt

--- module/getlist/getlist.js
+++ module/getlist/getlist.js
@@ -11,24 +11,8 @@ function GetList(){
         'content-type': 'application/json;odata=verbose',
         },
         success: function (data) {
-            var temp = data.d.results;
-            
-            if(ListName == 'title'){
-                for(i=0 ; i < temp.length ; i++){
-                    text += temp[i].Title + '\n';
-                }
-            }else{
-                for(i=0 ; i < temp.length ; i++){
-                    if(ListName.toUpperCase() == temp[i].Title.toUpperCase()){
-                        text += PackTextRow(40,temp[i]) + '\n';
-                    }else if(i == (temp.length-1)){
-                        text = 'List ' + ListName + ' is not found.\n';
-
-                    }
-                    
-                }
-            }   
-            
+            text = data.d.results;
+      
         },
 
         error: {}
