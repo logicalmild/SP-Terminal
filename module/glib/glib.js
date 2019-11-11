cc7fe7465469dedda54554873cbd7efa3623c5af
logicalmild <guide123@windowslive.com>
2019-11-11 13:15:39 +0700

    commit

--- module/glib/glib.js
+++ module/glib/glib.js
@@ -1,15 +1,28 @@
 function GlibStart(){
 
     var text = ''+
-               'Glib is Form & Workflow framework for SharePoint\n'+
+               'Glib is Form & Workflow framework for SharePoint\n\n'+
                '1. Introduction\n'+
                '2. Installation\n'+
                '3. Manual\n'+
-               ''+
+               '4. Open Glib on github\n'+
                ''+
                ''+
                '';
     terminal.clear();
     terminal.echo(text);
+
+}
+
+function GlibIntro(){
+
+}
+
+
+function GlibInstall(){
+
+}
+
+function GlibManual(){
 
 }
